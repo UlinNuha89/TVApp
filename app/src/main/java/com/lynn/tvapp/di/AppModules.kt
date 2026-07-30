cc64b757cbb34eb7fc6794da223b5bc6283f7934
UlinNuha89 <unuha89.un@gmail.com>
2026-07-30 08:11:26 +0700

    initial commit

---
+++ app/src/main/java/com/lynn/tvapp/di/AppModules.kt
@@ -0,0 +1,13 @@
+package com.lynn.tvapp.di
+
+import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.androidx.viewmodel.dsl.viewModelOf
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+object AppModules {
+    val modules = listOf<Module>(
+
+    )
+}
